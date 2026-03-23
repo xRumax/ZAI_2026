60ee179d454a096367af8798a0d3236a3d4a2592
xRumax <167364@student.uwm.edu.pl>
2026-03-23 16:58:08 +0100

    feat: Generic views. Implement List View and DetailView for all task module

---
+++ app/chores/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TaskListView, TaskDetailView
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name = "task_list"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name = "task_detail")
+]
+
+
--- app/chores/views.py
+++ app/chores/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
-# Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = "chores/chore_list.html"
+    context_object_name = "tasks"
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "chores/chore_detail.html"
+    context_object_name = "task"
+
+    
