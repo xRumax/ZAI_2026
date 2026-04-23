9e230d89b359bdd915d7f2c548b9ee1fc7e1bd9f
xRumax <167364@student.uwm.edu.pl>
2026-04-23 12:13:35 +0200

    feat: Create new validation for due_date field

--- app/chores/forms.py
+++ app/chores/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Task, Project
 from django.core.exceptions import ValidationError
-
+import datetime
 class TaskForm(ModelForm):
     class Meta:
         model = Task
@@ -13,7 +13,13 @@ class TaskForm(ModelForm):
         if "test" in title.lower():
             raise ValidationError("Tytuł nie może zawierać słowa 'test'.")
         return title.capitalize()
-
+    
+    def clean_due_date(self):
+        date_now = datetime.date.today()
+        due_date = self.cleaned_data.get('due_date')
+        if due_date < date_now:
+            raise ValidationError("Data nie może być w przeszłości")
+        return due_date
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
